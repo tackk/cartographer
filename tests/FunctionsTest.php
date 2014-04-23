9e8b153243a7ac84ae20284ad6f6f73d9febb9b9
Dan Horrigan <dan@dhorrigan.com>
2014-04-23 17:04:50 -0400

    Seperate the Function assertions into multiple tests. Add ArrayAccess test.

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -5,19 +5,33 @@ use Tackk\Cartographer;
 
 class FunctionsTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetProperty()
+    public function testGetPropertyFromArray()
     {
         $entry = ['foo' => 'bar'];
         $this->assertEquals('bar', Cartographer\get_property($entry, 'foo'));
         $this->assertEquals(null, Cartographer\get_property($entry, 'foobar'));
         $this->assertEquals(false, Cartographer\get_property($entry, 'foobar', false));
+    }
 
+    public function testGetPropertyFromObject()
+    {
         $entry = new stdClass();
         $entry->foo = 'bar';
         $this->assertEquals('bar', Cartographer\get_property($entry, 'foo'));
         $this->assertEquals(null, Cartographer\get_property($entry, 'foobar'));
         $this->assertEquals(false, Cartographer\get_property($entry, 'foobar', false));
+    }
 
+    public function testGetPropertyFromArrayAccessObject()
+    {
+        $entry = new ArrayIterator(['foo' => 'bar']);
+        $this->assertEquals('bar', Cartographer\get_property($entry, 'foo'));
+        $this->assertEquals(null, Cartographer\get_property($entry, 'foobar'));
+        $this->assertEquals(false, Cartographer\get_property($entry, 'foobar', false));
+    }
+
+    public function testGetPropertyFromNonObject()
+    {
         $entry = 'foo';
         $this->assertEquals(null, Cartographer\get_property($entry, 'foo'));
         $this->assertEquals(false, Cartographer\get_property($entry, 'foobar', false));
