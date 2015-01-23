ea23817aa6de12b016119d75945264ab88b1794f
Yury Druzhkov <bad1lamer@gmail.com>
2015-01-23 09:36:52 +0300

    ChangeFrequency class not found, when using only sitemap index

---
+++ src/ChangeFrequency.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tackk\Cartographer;
+
+class ChangeFrequency
+{
+    const ALWAYS  = 'always';
+    const HOURLY  = 'hourly';
+    const DAILY   = 'daily';
+    const WEEKLY  = 'weekly';
+    const MONTHLY = 'monthly';
+    const YEARLY  = 'yearly';
+    const NEVER   = 'never';
+}
--- src/Sitemap.php
+++ src/Sitemap.php
@@ -2,17 +2,6 @@
 
 namespace Tackk\Cartographer;
 
-class ChangeFrequency
-{
-    const ALWAYS  = 'always';
-    const HOURLY  = 'hourly';
-    const DAILY   = 'daily';
-    const WEEKLY  = 'weekly';
-    const MONTHLY = 'monthly';
-    const YEARLY  = 'yearly';
-    const NEVER   = 'never';
-}
-
 class Sitemap extends AbstractSitemap
 {
     protected function getRootNodeName()
